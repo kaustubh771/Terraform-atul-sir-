fe95edc1882439069b1caecb3b760aad90fca14f
kaustubh771 <kaustubhtyhakare0709@gmail.com>
2022-09-21 18:11:12 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
   access_key = "AKIA55SK3K2RJ7QFD6EY"
   secret_key = "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"
 }
